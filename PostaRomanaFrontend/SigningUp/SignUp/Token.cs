8dcd79518685dc8affb5c7fc067e145ee6438020
ioananicolae8 <ioananicolae12312@gmail.com>
2021-10-29 13:32:27 +0300

    call - Token Form

--- PostaRomanaFrontend/SigningUp/SignUp/Token.cs
+++ PostaRomanaFrontend/SigningUp/SignUp/Token.cs
@@ -1,13 +1,26 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Windows.Forms;
 
 namespace PostaRomanaFrontend.SignUp
 {
     public partial class Token : Form
     {
+        static HttpClient client = new HttpClient();
         public Token()
         {
             InitializeComponent();
+
+            client.BaseAddress = new Uri("https://localhost:5001/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                   new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public class ToSend
+        {
+            public string token { get; set; }
         }
 
         private void bt_Exit_Click(object sender, System.EventArgs e)
@@ -19,6 +32,13 @@ namespace PostaRomanaFrontend.SignUp
         {
             MainPage.MainPage main = new MainPage.MainPage();
             main.Show();
+
+            var toSend = new ToSend()
+            {
+                token = tb_Token.Text.Trim()
+            };
+
+            client.PostAsJsonAsync("/api/Account/Token", toSend);
         }
 
         private void tb_Token_TextChanged(object sender, System.EventArgs e)
